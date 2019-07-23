30ebcd84b18dea8530dd8abffc7938034422ddaf
miujiageng <miujiageng@hiynn.com>
2019-07-23 18:17:52 +0800

    code

--- src/components/EditPad/EditPad.tsx
+++ src/components/EditPad/EditPad.tsx
@@ -1,11 +1,20 @@
+/**
+ * markdown 编辑器
+ */
 import './editPad.scss'
 import React from 'react'
+import Markdown from 'react-markdown'
+import CodeMirror from 'react-codemirror'
 
 export default function EditPad(props: any) {
 
+  const options = {
+    mode: 'javascript'
+  }
+
   return (
     <div className={'edit-pad'}>
-      
+      <CodeMirror className={'edit'} value={'let a = 10'} autoFocus={true} options = {options}  />
     </div>
   )
 }
