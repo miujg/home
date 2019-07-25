574c2465336ddcc14c2e97fc1ef540f5870c2900
miujiageng <miujiageng@hiynn.com>
2019-07-25 17:33:12 +0800

    发现无限请求问题

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -4,19 +4,28 @@ import axios from 'src/tool/axios'
 import {Article} from 'coms/index'
 
 export default function Main(props:any) {
-  const articleTemp = [1,1,1,1,1,1,1,1]
+  const articleTemp = [1,1,1]
 
   const [articles, setArticles] = useState([])
   useEffect(() =>{
+    
+  })
+
+  const getData = () =>{
     axios.get('/article')
       .then((result:any) => {
         if(result.data.code) setArticles(result.data.result)
       })
-  })
+  }
 
   return (
     <div className={'main'}>
       <Article article={articles[0]} />        
+      {
+        articles.map((item:any, i) => {
+          <Article key={`ar${i}`} article={item} />
+        })   
+      }
       { 
         articleTemp.map((item, i) => (
           <Article key={i} />
